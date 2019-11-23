8415e6ef62e4f09fb8818c996432bce83ab306aa
TSStudio <TSS@mail.tmysam.top>
2019-11-23 10:18:42 +0800

    更新

---
+++ js/frame.js
@@ -0,0 +1,12 @@
+//init
+var sconsole=document.getElementById("console");
+function closec(sconsole){
+    sconsole.style.display="none";
+}
+function openc(sconsole){
+    sconsole.style.display="block";
+}
+var bp = document.createElement('script');
+bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(bp, s);
