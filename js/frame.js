4c23309c1e076b13af9df08528940ea89bb02adc
TSStudio <TSS@mail.tmysam.top>
2019-12-22 15:25:49 +0800

    some edit

--- js/frame.js
+++ js/frame.js
@@ -10,3 +10,10 @@ var bp = document.createElement('script');
 bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
 var s = document.getElementsByTagName("script")[0];
 s.parentNode.insertBefore(bp, s);
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?34ec57feab1c370d25354bdcc124201f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
